5406d45c55be61125320b0c559fbe7f4356540f9
Luan Lemos <ipsluan@icloud.com>
2026-04-26 18:37:48 -0300

    feat(backend): add route for create user

--- backend/src/controllers/user/CreateUserController.ts
+++ backend/src/controllers/user/CreateUserController.ts
@@ -6,7 +6,8 @@ class CreateUserController {
     const { name, email, password } = req.body;
 
     const createUserService = new CreateUserService();
-    const user = await createUserService.execute();
+    const user = await createUserService.execute({ name, email, password });
+
     return res.json(user);
   }
 }
--- backend/src/services/user/CreateUserService.ts
+++ backend/src/services/user/CreateUserService.ts
@@ -1,6 +1,51 @@
+import { prismaClient } from "../../prisma";
+import { hash } from "bcryptjs";
+
+interface UserRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
 class CreateUserService {
-  async execute() {
-    return { ok: true };
+  async execute({ name, email, password }: UserRequest) {
+    if (!name) {
+      throw new Error("Name is required");
+    }
+
+    if (!email) {
+      throw new Error("Email is required");
+    }
+
+    if (!password) {
+      throw new Error("Password is required");
+    }
+
+    const userAlreadyExists = await prismaClient.user.findFirst({
+      where: {
+        email: email,
+      },
+    });
+
+    if (userAlreadyExists) {
+      throw new Error("Email already exists");
+    }
+
+    const passwordHash = await hash(password, 8);
+    const user = await prismaClient.user.create({
+      data: {
+        name: name,
+        email: email,
+        password: passwordHash,
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+      },
+    });
+
+    return user;
   }
 }
 
