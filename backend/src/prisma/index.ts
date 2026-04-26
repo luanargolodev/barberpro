0e8d4b7ec75955296355c3befc67a8c1d24196e3
Luan Lemos <ipsluan@icloud.com>
2026-04-26 13:03:21 -0300

    feat(backend): add prisma and create controllers and services

---
+++ backend/src/prisma/index.ts
@@ -0,0 +1,8 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../generated/prisma/client";
+
+const prismaClient = new PrismaClient({
+  adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL }),
+});
+
+export { prismaClient };
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,8 @@
 import { Router, Request, Response } from "express";
+import { CreateUserController } from "./controllers/user/CreateUserController";
 
 const router = Router();
 
-router.get("/test", (req: Request, res: Response) => {
-  return res.json({ ok: true });
-});
+router.post("/users", new CreateUserController().handle);
 
 export { router };
